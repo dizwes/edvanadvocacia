77720a51caabd5095b2dc061ac3649526be682c5
dizwes <32206180+dizwes@users.noreply.github.com>
2017-09-22 16:08:46 -0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'ea-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ea';
+  public ngOnInit(){
+    $(window).scroll(function() {
+      if ($("nav").offset().top > 50) {
+          $(".navbar-fixed-top").addClass("top-nav-collapse gold");
+      } else {
+          $(".navbar-fixed-top").removeClass("top-nav-collapse gold");
+      }
+  });
+  }
+
 }
