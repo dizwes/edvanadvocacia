ad9c997983dec440d2f3389817da9046a34622bd
Wesley Morais de Oliveira <wesley.oliveira@sodit.com.br>
2017-09-27 14:32:22 -0300

    Alteração do hamburguer e pequenos ajustes no layout responsivo

--- src/app/mobile-menu/mobile-menu.component.ts
+++ src/app/mobile-menu/mobile-menu.component.ts
@@ -11,11 +11,29 @@ export class MobileMenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $(".menu-toggle").on('click', function() {
-      $(this).toggleClass("on");
-      $('.menu-section').toggleClass("on");
-      $("nav ul").toggleClass('hidden');
-    });
-  }
+    $(document).ready(function(){
+   $(".MenuIcon").click(function(){
+        $(".MainMenu").css("left","0px");
+        function showMenu(){
+            $(".MainMenu").css("-webkit-clip-path","polygon(0 0,100% 0,100% 100%,0% 100%)");
+            $(".MenuIcon").animate({right:'-100'},300);
+        }
+        setTimeout(showMenu,100);
+   });
 
-}
+    $(".close").click(function(){
+            $(".MainMenu").css("-webkit-clip-path","polygon(0 0,0% 0,100% 100%,0% 100%)");
+            function hideMenu(){
+                    $(".MainMenu").css("left","-300px");
+                $(".MenuIcon").animate({right:'50'},300);
+            }
+        setTimeout(hideMenu,300);
+
+        function originalLayout(){
+            $(".MainMenu").css("-webkit-clip-path","polygon(0 0,100% 0,0% 100%,0% 100%)");
+        }
+        setTimeout(originalLayout,600);
+    });
+});
+
+    }}
